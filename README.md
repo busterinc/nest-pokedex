b45278dcf7b1c398497cd0df282a412307723f77
busterinc <angel.rcaa@gmail.com>
2026-01-20 17:33:53 -0600

    Se configuran variables de entorno

--- README.md
+++ README.md
@@ -7,37 +7,50 @@
 # Ejecutar en desarrollo
 
 1. clonar el repositorio
+
 2. Ejecutar
 ```bash
 yarn install
 ```
+
 3. Tener Nest CLI instalado
 ```bash
 yarn add -g @nestjs/cli
 ```
+
 4. Levantar la base de datos
 ```bash
 docker-compose up -d
 ```
-5.Instalar mondodb nest
+
+5. Instalar mondodb nest
 ```bash
 yarn add @nestjs/mongoose mongoose
 ```
-6.Crear Moculo
+
+6. Crear Moculo
 ```bash
 nest g mo common
 ```
-7.Crear Pipes Personalizados
+
+7. Crear Pipes Personalizados
 ```bash
 nest g pi common/pipes/parseMongoId --no-spec
 ```
+
 8. Generar un Seed
 ```bash
 nest g res seed --no-spec
 ```
+
 9. Reconstruir la DB con un seed
 ```bash
 http://localhost:3000/api/v2/seed
+```
+
+10. Añadir variables de entorno
+```bash
+yarn add @nestjs/config
 ```
 
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,18 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
+    MongooseModule.forRoot(process.env.MONGODB || ''),
 
     PokemonModule,
 
@@ -22,4 +25,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
