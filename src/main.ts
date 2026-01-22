c6989d84a61c8e53a9819a8abb6ccd2c8dd929dd
busterinc <angel.rcaa@gmail.com>
2026-01-21 18:18:37 -0600

    Fix error to deploy

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application running on port ${process.env.PORT || 3000}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application running on port ${port}`);
 }
 bootstrap();
